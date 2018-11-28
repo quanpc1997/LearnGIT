8a929bfe50c556d63149f94f1fba51000869a4cb
unknown <skybyte0297@gmail.com>
2018-11-28 10:46:37 +0700

    Xu ly anh

---
+++ main.py
@@ -0,0 +1,122 @@
+import cv2 as cv
+
+max_value = 255
+max_value_H = 360//2
+
+low_H = 0
+low_S = 0
+low_V = 0
+low_E = 0
+
+high_H = max_value_H
+high_S = max_value
+high_V = max_value
+high_E = max_value
+
+window_capture_name = 'Video Capture'
+window_detection_name = 'Object Detection'
+window_detection_edge_name = "Edge Detection"
+
+low_H_name = 'Low H'
+low_S_name = 'Low S'
+low_V_name = 'Low V'
+low_E_name = 'Low Edge'
+
+high_H_name = 'High H'
+high_S_name = 'High S'
+high_V_name = 'High V'
+high_E_name = 'High Edge'
+
+# Change Hue
+def on_low_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    low_H = val
+    low_H = min(high_H-1, low_H)
+    cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
+def on_high_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    high_H = val
+    high_H = max(high_H, low_H+1)
+    cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
+
+# Change Saturation
+def on_low_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    low_S = val
+    low_S = min(high_S-1, low_S)
+    cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
+def on_high_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    high_S = val
+    high_S = max(high_S, low_S+1)
+    cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
+
+# Change Value
+def on_low_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    low_V = val
+    low_V = min(high_V-1, low_V)
+    cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
+def on_high_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    high_V = val
+    high_V = max(high_V, low_V+1)
+    cv.setTrackbarPos(high_V_name, window_detection_name, high_V)
+
+# Change Edge
+def on_low_E_thresh_trackbar(val):
+    global low_E
+    global high_E
+    low_V = val
+    low_V = min(high_E-1, low_E)
+    cv.setTrackbarPos(low_E_name, window_detection_edge_name, low_E)
+def on_high_E_thresh_trackbar(val):
+    global low_E
+    global high_E
+    high_E = val
+    high_E = max(high_E, low_E+1)
+    cv.setTrackbarPos(high_E_name, window_detection_edge_name, high_E)
+
+def get_person(val):
+    pass
+
+
+cap = cv.imread("3.jpg", 1)
+# Define Name Windows
+cv.namedWindow(window_capture_name)
+cv.namedWindow(window_detection_name)
+cv.namedWindow(window_detection_edge_name)
+
+# Windows Detect Mask
+cv.createTrackbar(low_H_name, window_detection_name , low_H, max_value_H, on_low_H_thresh_trackbar)
+cv.createTrackbar(high_H_name, window_detection_name , high_H, max_value_H, on_high_H_thresh_trackbar)
+cv.createTrackbar(low_S_name, window_detection_name , low_S, max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar(high_S_name, window_detection_name , high_S, max_value, on_high_S_thresh_trackbar)
+cv.createTrackbar(low_V_name, window_detection_name , low_V, max_value, on_low_V_thresh_trackbar)
+cv.createTrackbar(high_V_name, window_detection_name , high_V, max_value, on_high_V_thresh_trackbar)
+
+# Windows Detect Edge
+cv.createTrackbar(low_E_name, window_detection_edge_name , low_E, max_value, on_low_E_thresh_trackbar)
+cv.createTrackbar(high_E_name, window_detection_edge_name , high_E, max_value, on_high_E_thresh_trackbar)
+
+while True:
+    
+    frame_HSV = cv.cvtColor(cap, cv.COLOR_BGR2HSV)
+    frame_BGR2BGRA = cv.cvtColor(cap, cv.COLOR_BGR2BGRA)
+    frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    frame_canny = cv.Canny(cap, low_E, high_E)
+    
+    
+    cv.imshow(window_capture_name, cap)
+    cv.imshow(window_detection_name, frame_threshold)
+    cv.imshow(window_detection_edge_name, frame_canny)
+    
+    key = cv.waitKey(30)
+    if key == ord('q') or key == 27:
+        break
